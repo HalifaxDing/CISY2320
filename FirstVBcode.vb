c7492ad5c64514f4c1ddd57c9b39db7bf907c24e
HalifaxDing <95089293+HalifaxDing@users.noreply.github.com>
2024-09-05 12:08:59 -0300

    Create FirstVBcode.vb

---
+++ FirstVBcode.vb
@@ -0,0 +1,6 @@
+    Sub Main()
+        Dim firstName As String
+        Console.WriteLine("Enter your firstname: ")
+        firstName = Console.Readline()
+        Console.WriteLine("Hello, " & firstName & "!")
+    End Sub
